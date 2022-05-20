84dc0147c7272d2381461d877dddba49f23bea63
felipedias87 <105897491+felipedias87@users.noreply.github.com>
2022-05-19 23:09:32 -0300

    Remover Arquivos de um Diretorio especificado
    Se trata de um pequeno script em Python que desenvolvi , bem simples , você entra com o PATH de entrada , ou seja com o diretório de entrada e um diretório de saída , ele copia os arquivos com a extensão escolhida , de um diretório para o outro e remove os arquivos do PATH de entrada.

---
+++ removeArquivos.py
@@ -0,0 +1,23 @@
+import os
+import shutil
+src = str(input("Digite o diretório de onde deseja deletar os arquivos:\n"))
+trg = str(input("Digite o diretório para onde deseja mover os arquivos:\n"))
+extensao = str(input("Qual a extensão de arquios que deseja remover?\n"))
+ext = "." + extensao
+ext.lstrip()
+guardaNum = []
+files = os.listdir(src)
+for fname in files:
+    extensao = os.path.splitext(fname)
+    numArquivos = 0
+    if extensao[1] == ext:
+        numArquivos = numArquivos + 1
+        guardaNum.append(numArquivos)
+        print(fname," - Arquivo movido")
+        shutil.copy2(os.path.join(src, fname), trg)
+        arquivoTotal = src+fname
+        #if os.path.exists(arquivoTotal):
+        os.remove(arquivoTotal)
+        #shutil.move(src, trg)
+print (f"\nO diretório ({src}) não contem arquivos com a extensão ({ext}) para apagar!! \n" if not guardaNum else f"Total de {len(guardaNum)} arquivos foram apagados e movidos para a pasta {trg}")
+os.system("pause")
